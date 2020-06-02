b5c357d9bf766a1c74aad265e4e78a342aa1fd78
Randy Sojo <72002@hmoservisalud.com.ve>
2020-06-02 10:53:55 -0400

    Segundo respaldo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Styde.net
+                    Styde.net..
                 </div>
                 
             </div>
